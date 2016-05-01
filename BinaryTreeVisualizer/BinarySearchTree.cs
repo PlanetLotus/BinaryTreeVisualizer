c56e09d9958b20c25d725f65bd4775fa577582dd
Matt Messmer <staind2523@gmail.com>
2016-05-01 14:40:29 -0700

    Initial commit.

---
+++ BinaryTreeVisualizer/BinarySearchTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeVisualizer
+{
+    public sealed class BinarySearchTree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public void Insert(T data)
+        {
+        }
+
+        public void Delete(T data)
+        {
+        }
+
+        public T GetMin()
+        {
+            if (Root == null)
+            {
+                return default(T);
+            }
+
+            Node<T> node = Root;
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+
+            return node.Data;
+        }
+
+        public T GetMax()
+        {
+            if (Root == null)
+            {
+                return default(T);
+            }
+
+            Node<T> node = Root;
+            while (node.Right != null)
+            {
+                node = node.Right;
+            }
+
+            return node.Data;
+        }
+    }
+}
---
+++ BinaryTreeVisualizer/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace BinaryTreeVisualizer
+{
+    public sealed class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+    }
+}
